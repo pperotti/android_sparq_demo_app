9a61b5d6e94258e8110f1b8087197c309e12e601
Pablo Perotti <pablo.perotti@gmail.com>
2025-05-07 17:39:35 -0300

    Update project dependencies to latest versions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
 
+    // Out-Of-The-Box libraries
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,7 +50,12 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.activity.ktx)
 
+    // ViewModels
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    // Tests
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
